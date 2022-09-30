e757a99f351361ab2b3930d44a99acbefc40c274
Camilo Castro <castroh24@gmail.com>
2022-09-30 16:43:17 -0600

    gitIgnore y script de insertart agregado

---
+++ InsertarDatosScripts.sql
@@ -0,0 +1,17 @@
+Insert into Pais(IdPais, PaisNombre)
+Values('NIC', 'Nicaragua')
+GO
+Insert into Pais(IdPais, PaisNombre)
+Values('USA', 'Estados Unidos')
+GO
+Insert into Pais(IdPais, PaisNombre)
+Values('CAN', 'Canada')
+GO
+INSERT INTO Pais(IdPais, PaisNombre)
+VALUES ('CHL','Chile')
+GO
+INSERT INTO Pais(IdPais, PaisNombre)
+VALUES('ECU','Ecuador')
+GO
+Select * from Pais
+Delete from Pais where IdPais = 'CA'
