9cacef6c06b43c6aa57944eadf27f28eb8044bd8
Camilo Castro <castroh24@gmail.com>
2022-09-30 20:00:28 -0600

    Se agrego schema cliente y H.R

--- InsertarDatosScripts.sql
+++ InsertarDatosScripts.sql
@@ -1,3 +1,4 @@
+-- Agregando datos en la tabla Pais
 Insert into Pais(IdPais, PaisNombre)
 Values('NIC', 'Nicaragua')
 GO
@@ -13,5 +14,13 @@ GO
 INSERT INTO Pais(IdPais, PaisNombre)
 VALUES('ECU','Ecuador')
 GO
+INSERT INTO Pais(IdPais, PaisNombre)
+VALUES('SLV','El Salvador')
+GO
+INSERT INTO Pais(IdPais, PaisNombre)
+VALUES('FRA', 'Francia')
+GO
+--Agregando datos en la tabla Paciente
+
 Select * from Pais
 Delete from Pais where IdPais = 'CA'
