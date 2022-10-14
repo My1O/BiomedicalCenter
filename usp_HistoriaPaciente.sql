343d5986820f878020be5c5220c075e358e8a4fd
Camilo Castro <castroh24@gmail.com>
2022-10-14 08:58:57 -0600

    filtro Historia paciente actualizado

--- usp_HistoriaPaciente.sql
+++ usp_HistoriaPaciente.sql
@@ -4,26 +4,30 @@ CREATE PROC usp_HistoriaPaciente(
 AS
 BEGIN
 IF EXISTS(
-SELECT * FROM clientedbo.Paciente pa
-INNER JOIN HistoriaPaciente HP
-ON pa.IdPaciente = HP.IdPaciente
-INNER JOIN Historia H
-ON H.IdHistoria = HP.IdHistoria
-INNER JOIN MedicoEspecialidad ME
-ON ME.IdMedico = HP.IdMedico
-INNER JOIN hrdbo.Medico M
-ON M.IdMedico = HP.IdMedico
-WHERE pa.IdPaciente = @IdPaciente)
-SELECT * FROM clientedbo.Paciente pa
-INNER JOIN HistoriaPaciente HP
-ON pa.IdPaciente = HP.IdPaciente
-INNER JOIN Historia H
-ON H.IdHistoria = HP.IdHistoria
-INNER JOIN MedicoEspecialidad ME
-ON ME.IdMedico = HP.IdMedico
-INNER JOIN hrdbo.Medico M
-ON M.IdMedico = HP.IdMedico
-WHERE pa.IdPaciente = @IdPaciente
+				SELECT *
+				FROM clientedbo.Paciente pa
+				INNER JOIN HistoriaPaciente HP
+				ON pa.IdPaciente = HP.IdPaciente
+				INNER JOIN Historia H
+				ON H.IdHistoria = HP.IdHistoria
+				INNER JOIN MedicoEspecialidad ME
+				ON ME.IdMedico = HP.IdMedico
+				INNER JOIN hrdbo.Medico M
+				ON M.IdMedico = HP.IdMedico
+				WHERE pa.IdPaciente = @IdPaciente
+		)
+	SELECT		 pa.IdPaciente, pa.DNI, pa.Nombre, pa.Apellido,
+				pa.FechaNacimiento,pa.IdPais, HP.IdHistoria, H.FechaHistoria, ME.IdMedico,ME.IdEspecialidad,
+				ME.Descripcion, M.Nombre, M.Apellido FROM clientedbo.Paciente pa
+	INNER JOIN HistoriaPaciente HP
+	ON pa.IdPaciente = HP.IdPaciente
+	INNER JOIN Historia H
+	ON H.IdHistoria = HP.IdHistoria
+	INNER JOIN MedicoEspecialidad ME
+	ON ME.IdMedico = HP.IdMedico
+	INNER JOIN hrdbo.Medico M
+	ON M.IdMedico = HP.IdMedico
+	WHERE pa.IdPaciente = @IdPaciente
 ELSE
 	PRINT 'No existe historia clinica para el paciente ingresado'
 END
