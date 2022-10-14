d4d5d5f76aa8c8a93ed7a2bca140693800273652
Camilo Castro <castroh24@gmail.com>
2022-10-14 00:49:18 -0600

    validacion agregada al USP historia paciente

--- usp_HistoriaPaciente.sql
+++ usp_HistoriaPaciente.sql
@@ -3,6 +3,17 @@ CREATE PROC usp_HistoriaPaciente(
 		@IdPaciente int)
 AS
 BEGIN
+IF EXISTS(
+SELECT * FROM clientedbo.Paciente pa
+INNER JOIN HistoriaPaciente HP
+ON pa.IdPaciente = HP.IdPaciente
+INNER JOIN Historia H
+ON H.IdHistoria = HP.IdHistoria
+INNER JOIN MedicoEspecialidad ME
+ON ME.IdMedico = HP.IdMedico
+INNER JOIN hrdbo.Medico M
+ON M.IdMedico = HP.IdMedico
+WHERE pa.IdPaciente = @IdPaciente)
 SELECT * FROM clientedbo.Paciente pa
 INNER JOIN HistoriaPaciente HP
 ON pa.IdPaciente = HP.IdPaciente
@@ -13,6 +24,8 @@ ON ME.IdMedico = HP.IdMedico
 INNER JOIN hrdbo.Medico M
 ON M.IdMedico = HP.IdMedico
 WHERE pa.IdPaciente = @IdPaciente
+ELSE
+	PRINT 'No existe historia clinica para el paciente ingresado'
 END
 
---EXEC usp_HistoriaPaciente 4
+-- EXEC usp_HistoriaPaciente 4
